a1eaad6aa74b247f89f44e1189bc16fc5c2f0118
Jan Poniecki <jponieck@c2r2s4.42warsaw.pl>
2024-10-24 15:09:35 +0200

    printing_env

--- src/cgi/mycgi.py
+++ src/cgi/mycgi.py
@@ -1,4 +1,10 @@
 import os
-import urlib.parse
+import sys
 
-print("hello")
+query_string = os.getenv("QUERY_STRING", "")
+content_length = int(os.getenv("CONTENT_LENGTH", 0))
+request_method = os.getenv("REQUEST_METHOD", "")
+print(f"quey string is {query_string}")
+print(f"content length is {content_length}")
+print(f"request method is {request_method}")
+
