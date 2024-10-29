3ab1ca19ac0ad7d9ca9eec959d06ffb51a752336
Bartosz <bartekbkotwica@gmail.com>
2024-10-29 10:14:27 +0100

    merging CGI and dev0.2, on main branch

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,13 @@
 #include "../header/read_conf.hpp"
 
-int main() {
+int main()
+{
     int server_fd, new_socket;
     struct sockaddr_in address;
     int opt = 1;
     int addrlen = sizeof(address);
     char buffer[1024] = {0};
 
-    // Tworzenie gniazda
     // Tworzenie gniazda
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (server_fd == 0) {
@@ -16,34 +16,13 @@ int main() {
     }
 
     // Konfiguracja socketu
-    // Konfiguracja socketu
     setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
 
     // Konfiguracja serwera
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(8080);
-    // Konfiguracja serwera
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(8080);
 
-    // Bindowanie socketu
-    bind(server_fd, (struct sockaddr *)&address, sizeof(address));
-
-    // Nasłuchiwanie
-    listen(server_fd, 3);
-
-    std::cout << "Serwer nasłuchuje na porcie 8080\n";
-
-    while (true) {
-        // Akceptowanie połączenia
-        new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
-
-        if (new_socket < 0) {
-            std::cerr << "Błąd połączenia\n";
-            return -1;
-        }
     // Bindowanie socketu
     bind(server_fd, (struct sockaddr *)&address, sizeof(address));
 
