25dd88c966d0a49c5eca86ed4f71024a745bcb16
Bartosz <bartekbkotwica@gmail.com>
2024-10-29 09:59:02 +0100

    clean the folders and the files in CGI branch

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,4 @@
-#include <iostream>
-#include <string.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <cstdlib>
+#include "../header/read_conf.hpp"
 
 int main() {
     int server_fd, new_socket;
@@ -48,102 +41,76 @@ int main() {
 
         // Odbieranie danych od przeglądarki
         read(new_socket, buffer, 1024);
-        std::cout << "Odebrano żądanie:\n" << buffer << std::endl;
-
         // Sprawdzanie, czy to jest żądanie CGI (np. POST)
-        if (strstr(buffer, "POST /") != nullptr) {
-            // Znalezienie Content-Length (długość danych POST)
-            char *content_length_str = strstr(buffer, "Content-Length:");
-            int content_length = 0;
-            if (content_length_str != nullptr) {
-                sscanf(content_length_str, "Content-Length: %d", &content_length);
-            }
 
-            // Tworzenie bufora na dane POST
-            char *post_data = new char[content_length + 1]();
-            read(new_socket, post_data, content_length);
+        // if (strstr(buffer, "POST /") != nullptr) {
+        //     // Znalezienie Content-Length (długość danych POST)
+        //     char *content_length_str = strstr(buffer, "Content-Length:");
+        //     int content_length = 0;
+        //     if (content_length_str != nullptr) {
+        //         sscanf(content_length_str, "Content-Length: %d", &content_length);
+        //     }
 
-            // Utworzenie procesu potomnego dla skryptu CGI
-            pid_t pid = fork();
-            if (pid == 0) {  // Proces potomny
-                // Ustawienie zmiennych środowiskowych dla skryptu CGI
-                setenv("REQUEST_METHOD", "POST", 1);
-                setenv("CONTENT_LENGTH", std::to_string(content_length).c_str(), 1);
+        //     // Tworzenie bufora na dane POST
+        //     char *post_data = new char[content_length + 1]();
+        //     read(new_socket, post_data, content_length);
 
-                // Przekazywanie danych POST przez standardowe wejście (stdin)
-                dup2(new_socket, STDIN_FILENO);
+        //     // Utworzenie procesu potomnego dla skryptu CGI
+        //     pid_t pid = fork();
+        //     if (pid == 0) {  // Proces potomny
+        //         // Ustawienie zmiennych środowiskowych dla skryptu CGI
+        //         setenv("REQUEST_METHOD", "POST", 1);
+        //         setenv("CONTENT_LENGTH", std::to_string(content_length).c_str(), 1);
 
-                // Uruchomienie skryptu CGI
-                const char *args[] = {"/path/to/cgi-bin/script.cgi", nullptr};  // Ścieżka do skryptu
-                execvp(args[0], (char *const *)args);
+        //         // Przekazywanie danych POST przez standardowe wejście (stdin)
+        //         dup2(new_socket, STDIN_FILENO);
 
-                // Jeśli execvp się nie powiedzie
-                exit(EXIT_FAILURE);
-            } else {  // Proces rodzica
-                // Oczekiwanie na zakończenie procesu CGI
-                waitpid(pid, nullptr, 0);
+        //         // Uruchomienie skryptu CGI
+        //         const char *args[] = {"/path/to/cgi-bin/script.cgi", nullptr};  // Ścieżka do skryptu
+        //         execvp(args[0], (char *const *)args);
 
-                // Zwracanie odpowiedzi HTTP (np. wyniku skryptu CGI)
+        //         // Jeśli execvp się nie powiedzie
+        //         exit(EXIT_FAILURE);
+        //     } else {  // Proces rodzica
+        //         // Oczekiwanie na zakończenie procesu CGI
+        //         waitpid(pid, nullptr, 0);
+
+        //         // Zwracanie odpowiedzi HTTP (np. wyniku skryptu CGI)
+        //         const char *http_response =
+        //             "HTTP/1.1 200 OK\r\n"
+        //             "Content-Type: text/html\r\n"
+        //             "Content-Length: 400\r\n"
+        //             "\n"
+        //             "<h1>Wynik skryptu CGI</h1>";
+        //         send(new_socket, http_response, strlen(http_response), 0);
+        //         std::cout << "Odpowiedź CGI została wysłana do klienta\n";
+        //     }
+
+        //     delete[] post_data;
+        // }
+
+        // Sprawdzanie żądania GET
+        if (strstr(buffer, "GET /") != nullptr)
+        {
+            if (met_get(buffer, new_socket))
+                return (1);
+        }
+
+        else {
+            std::cout << "inny case\n"; 
+                // Standardowa odpowiedź dla innych żądań
                 const char *http_response =
                     "HTTP/1.1 200 OK\r\n"
                     "Content-Type: text/html\r\n"
                     "Content-Length: 40\r\n"
                     "\n"
-                    "<h1>Wynik skryptu CGI</h1>";
+                    "<h1>Hello World :) </h1>";
                 send(new_socket, http_response, strlen(http_response), 0);
-                std::cout << "Odpowiedź CGI została wysłana do klienta\n";
+                std::cout << "Odpowiedź została wysłana do klienta\n";
             }
 
-            delete[] post_data;
-        } else if (strstr(buffer, "GET /") != nullptr) {
-            // Przykład dla metody GET (dane przekazywane przez QUERY_STRING)
-            char *query_string = strstr(buffer, "GET /");
-            if (query_string != nullptr) {
-                // Pobieranie wartości QUERY_STRING z URI
-                char *end_of_uri = strchr(query_string, ' ');
-                *end_of_uri = '\0';  // Odcinanie URI
-
-                // Utworzenie procesu potomnego
-                pid_t pid = fork();
-                if (pid == 0) {  // Proces potomny
-                    // Ustawienie QUERY_STRING w zmiennej środowiskowej
-                    setenv("QUERY_STRING", query_string + 12, 1);  // 12 to długość "/cgi-bin/"
-
-                    // Uruchomienie skryptu CGI
-                    const char *args[] = {"./cgi/mycgi.py", nullptr};  // Ścieżka do skryptu
-                    execvp(args[0], (char *const *)args);
-
-                    // Jeśli execvp się nie powiedzie
-                    exit(EXIT_FAILURE);
-                } else {  // Proces rodzica
-                    // Oczekiwanie na zakończenie procesu CGI
-                    waitpid(pid, nullptr, 0);
-
-                    // Zwracanie odpowiedzi HTTP
-                    const char *http_response =
-                        "HTTP/1.1 200 OK\r\n"
-                        "Content-Type: text/html\r\n"
-                        "Content-Length: 40\r\n"
-                        "\n"
-                        "<h1>Wynik skryptu CGI GET</h1>";
-                    send(new_socket, http_response, strlen(http_response), 0);
-                    std::cout << "Odpowiedź CGI GET została wysłana do klienta\n";
-                }
-            }
-        } else {
-            // Standardowa odpowiedź dla innych żądań
-            const char *http_response =
-                "HTTP/1.1 200 OK\r\n"
-                "Content-Type: text/html\r\n"
-                "Content-Length: 40\r\n"
-                "\n"
-                "<h1>Hello World :) </h1>";
-            send(new_socket, http_response, strlen(http_response), 0);
-            std::cout << "Odpowiedź została wysłana do klienta\n";
-        }
-
-        // Zamknięcie połączenia
-        close(new_socket);
+            // Zamknięcie połączenia
+            close(new_socket);
     }
 
     close(server_fd);
